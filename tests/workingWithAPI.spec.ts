88d8d32191a343fee1ca44781bf89db85af4de67
Abul Hasan <abul.hasan@cefalo.com>
2025-06-22 23:56:03 +0600

    Intercept API response and modify first data. After that verify modified data visible on UI

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -16,6 +16,24 @@ test.beforeEach(async ({ page }) => {
         })
     })
 
+    await page.route('*/**/api/articles*', async route => {
+        const response = await route.fetch();
+        const responseBody = await response.json();
+        responseBody.articles[0].title = 'This is a mocked article title';
+        responseBody.articles[0].description = 'This is a mocked article description';
+
+        await route.fulfill({
+            status: 200,
+            headers: {
+                'Cache-Control': 'no-cache, no-store, must-revalidate',
+                'Pragma': 'no-cache',
+                'Expires': '0'
+            },
+            contentType: 'application/json',
+            body: JSON.stringify(responseBody)
+        })
+    })
+
     await page.goto('https://conduit.bondaracademy.com/', {
         waitUntil: 'networkidle'
     });
@@ -40,4 +58,14 @@ test('tags should show mock data', async ({ page }) => {
 
     // Optional visual confirmation
     await page.screenshot({ path: 'tags-loaded.png' });
+})
+
+test('articles should show mock data', async ({ page }) => {
+    // 1. Check Text Directly on Page
+    await expect(page.getByText('This is a mocked article title')).toBeVisible();
+    await expect(page.getByText('This is a mocked article description')).toBeVisible();
+
+    // 2. Check Specific Container
+    await expect(page.locator('app-article-list h1').first()).toContainText('This is a mocked article title');
+    await expect(page.locator('app-article-list p').first()).toContainText('This is a mocked article description');
 })
